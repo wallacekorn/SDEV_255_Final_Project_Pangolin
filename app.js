9ca44218d77b32993b4e2b05138457616fb5e21b
Sean Richardson <srichardson191@ivytech.edu>
2023-12-03 02:20:56 -0500

    Added pages addCourse.ejs, editCourse.ejs, deleteCourse.ejs Updated app.js, faculty.ejs to handle crud
    From the faculty page, there is now an option to edit or delete each course.
    There is now an option to add a new course.
    All changes and additions to courses will be displayed on the course_page.ejs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.listen(3000);
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const courses = [
     {
@@ -34,6 +36,96 @@ const courses = [
         credits: '2'
     },
 ]
+
+// Display the faculty page with courses
+app.get('/faculty', (req, res) => {
+    res.render('faculty', { title: 'Faculty Home Page', courses });
+  });
+  
+  // Display the course edit page
+  app.get('/faculty/edit/:title', (req, res) => {
+    const courseTitle = req.params.title;
+    const course = courses.find(course => course.title === courseTitle);
+  
+    if (course) {
+      res.render('editCourse', { title: 'Edit Course', course });
+    } else {
+      res.status(404).render('404', { title: '404: Page Not Found' });
+    }
+  });
+  
+  // Handle course update
+  app.post('/faculty/edit/:title', (req, res) => {
+    const courseTitle = req.params.title;
+    const updatedCourse = {
+      title: req.body.title,
+      description: req.body.description,
+      subject: req.body.subject,
+      credits: req.body.credits,
+    };
+  
+    // Update the course in the courses array (replace with MongoDB update logic)
+    const index = courses.findIndex(course => course.title === courseTitle);
+    if (index !== -1) {
+      courses[index] = updatedCourse;
+      res.redirect('/faculty');
+    } else {
+      res.status(404).render('404', { title: '404: Page Not Found' });
+    }
+  });
+  
+  // Display the course deletion confirmation page
+  app.get('/faculty/delete/:title', (req, res) => {
+    const courseTitle = req.params.title;
+    const course = courses.find(course => course.title === courseTitle);
+  
+    if (course) {
+      res.render('deleteCourse', { title: 'Delete Course', course });
+    } else {
+      res.status(404).render('404', { title: '404: Page Not Found' });
+    }
+  });
+  
+  // Handle course deletion
+  app.post('/faculty/delete/:title', (req, res) => {
+    const courseTitle = req.params.title;
+  
+    // Remove the course from the courses array (replace with MongoDB delete logic)
+    const index = courses.findIndex(course => course.title === courseTitle);
+    if (index !== -1) {
+      courses.splice(index, 1);
+      res.redirect('/faculty');
+    } else {
+      res.status(404).render('404', { title: '404: Page Not Found' });
+    }
+  });
+  
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+
+  // Add this route to app.js
+app.get('/faculty/add-course', (req, res) => {
+    res.render('addCourse', { title: 'Add Course' });
+});
+
+app.post('/faculty/add-course', (req, res) => {
+    // Logic to handle adding a new course (replace with MongoDB logic)
+    const newCourse = {
+        title: req.body.title,
+        description: req.body.description,
+        subject: req.body.subject,
+        credits: req.body.credits,
+    };
+
+    // Add the new course to the courses array (replace with MongoDB logic)
+    courses.push(newCourse);
+
+    // Redirect to the faculty page after adding the course
+    res.redirect('/faculty');
+});
+
+
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home'}); 
